6f09a7c8c12b0994a8c09958b514d21016361a1c
Esmond <esmondmccain@gmail.com>
2025-02-13 12:37:22 -0600

    fix error

--- archive.php
+++ archive.php
@@ -9,45 +9,25 @@
 
 get_header();
 
-die();
 ?>
 
 	<main id="primary" class="archive-esmond site-main em-page-content">
 
-		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+			<?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post();
+        	get_template_part( 'template-parts/content', get_post_type() );
+            endwhile;
+        else :
+            _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+        endif;
+        ?>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
