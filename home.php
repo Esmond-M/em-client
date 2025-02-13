9baa44bdf670681271e65276902a3d45a4fbc608
Esmond <esmondmccain@gmail.com>
2025-02-13 09:29:56 -0600

    Create home.php

---
+++ home.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package em-client
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main em-client-blog">
+
+		<?php
+		if ( have_posts() ) :
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            <!--<img src=""/> -->
+			<?php emclient_post_thumbnail(); ?>
+			<header class="entry-header">
+				<?php
+				if ( is_singular() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
+
+				if ( 'post' === get_post_type() ) :
+					?>
+					<div class="entry-meta">
+						<?php
+						emclient_posted_on();
+						emclient_posted_by();
+						?>
+					</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
+			<div class="entry-content">
+				<?php
+				the_excerpt(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'em-client' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post( get_the_title() )
+					)
+				);
+
+				wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'em-client' ),
+						'after'  => '</div>',
+					)
+				);
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php emclient_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
+		</article><!-- #post-<?php the_ID(); ?> -->
+            <?php
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
