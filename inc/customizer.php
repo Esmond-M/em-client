75c5974164af6cc2a37126022e623831d275de90
Esmond <esmondmccain@gmail.com>
2025-09-03 17:50:58 -0500

    convert tabs to spaces

--- inc/customizer.php
+++ inc/customizer.php
@@ -6,52 +6,52 @@
  */
 
 class EmClient_Customizer {
-	/**
-	 * Register customizer settings and partials
-	 */
-	public static function register( $wp_customize ) {
-		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-		$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+    /**
+     * Register customizer settings and partials
+     */
+    public static function register( $wp_customize ) {
+        $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+        $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+        $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
-		if ( isset( $wp_customize->selective_refresh ) ) {
-			$wp_customize->selective_refresh->add_partial(
-				'blogname',
-				array(
-					'selector'        => '.site-title a',
-					'render_callback' => [ __CLASS__, 'partial_blogname' ],
-				)
-			);
-			$wp_customize->selective_refresh->add_partial(
-				'blogdescription',
-				array(
-					'selector'        => '.site-description',
-					'render_callback' => [ __CLASS__, 'partial_blogdescription' ],
-				)
-			);
-		}
-	}
+        if ( isset( $wp_customize->selective_refresh ) ) {
+            $wp_customize->selective_refresh->add_partial(
+                'blogname',
+                array(
+                    'selector'        => '.site-title a',
+                    'render_callback' => [ __CLASS__, 'partial_blogname' ],
+                )
+            );
+            $wp_customize->selective_refresh->add_partial(
+                'blogdescription',
+                array(
+                    'selector'        => '.site-description',
+                    'render_callback' => [ __CLASS__, 'partial_blogdescription' ],
+                )
+            );
+        }
+    }
 
-	/**
-	 * Render the site title for the selective refresh partial.
-	 */
-	public static function partial_blogname() {
-		bloginfo( 'name' );
-	}
+    /**
+     * Render the site title for the selective refresh partial.
+     */
+    public static function partial_blogname() {
+        bloginfo( 'name' );
+    }
 
-	/**
-	 * Render the site tagline for the selective refresh partial.
-	 */
-	public static function partial_blogdescription() {
-		bloginfo( 'description' );
-	}
+    /**
+     * Render the site tagline for the selective refresh partial.
+     */
+    public static function partial_blogdescription() {
+        bloginfo( 'description' );
+    }
 
-	/**
-	 * Enqueue customizer preview JS
-	 */
-	public static function preview_js() {
-		wp_enqueue_script( 'emclientcustomizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), defined('_S_VERSION') ? _S_VERSION : false, true );
-	}
+    /**
+     * Enqueue customizer preview JS
+     */
+    public static function preview_js() {
+        wp_enqueue_script( 'emclientcustomizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), defined('_S_VERSION') ? _S_VERSION : false, true );
+    }
 }
 
 add_action( 'customize_register', [ 'EmClient_Customizer', 'register' ] );
--- inc/jetpack.php
+++ inc/jetpack.php
@@ -7,57 +7,57 @@
  */
 
 class EmClient_Jetpack {
-	/**
-	 * Setup Jetpack theme supports
-	 */
-	public static function setup() {
-		// Add theme support for Infinite Scroll.
-		add_theme_support(
-			'infinite-scroll',
-			array(
-				'container' => 'main',
-				'render'    => [ __CLASS__, 'infinite_scroll_render' ],
-				'footer'    => 'page',
-			)
-		);
+    /**
+     * Setup Jetpack theme supports
+     */
+    public static function setup() {
+        // Add theme support for Infinite Scroll.
+        add_theme_support(
+            'infinite-scroll',
+            array(
+                'container' => 'main',
+                'render'    => [ __CLASS__, 'infinite_scroll_render' ],
+                'footer'    => 'page',
+            )
+        );
 
-		// Add theme support for Responsive Videos.
-		add_theme_support( 'jetpack-responsive-videos' );
+        // Add theme support for Responsive Videos.
+        add_theme_support( 'jetpack-responsive-videos' );
 
-		// Add theme support for Content Options.
-		add_theme_support(
-			'jetpack-content-options',
-			array(
-				'post-details' => array(
-					'stylesheet' => 'emclientstyle',
-					'date'       => '.posted-on',
-					'categories' => '.cat-links',
-					'tags'       => '.tags-links',
-					'author'     => '.byline',
-					'comment'    => '.comments-link',
-				),
-				'featured-images' => array(
-					'archive' => true,
-					'post'    => true,
-					'page'    => true,
-				),
-			)
-		);
-	}
+        // Add theme support for Content Options.
+        add_theme_support(
+            'jetpack-content-options',
+            array(
+                'post-details' => array(
+                    'stylesheet' => 'emclientstyle',
+                    'date'       => '.posted-on',
+                    'categories' => '.cat-links',
+                    'tags'       => '.tags-links',
+                    'author'     => '.byline',
+                    'comment'    => '.comments-link',
+                ),
+                'featured-images' => array(
+                    'archive' => true,
+                    'post'    => true,
+                    'page'    => true,
+                ),
+            )
+        );
+    }
 
-	/**
-	 * Custom render function for Infinite Scroll
-	 */
-	public static function infinite_scroll_render() {
-		while ( have_posts() ) {
-			the_post();
-			if ( is_search() ) :
-				get_template_part( 'template-parts/content', 'search' );
-			else :
-				get_template_part( 'template-parts/content', get_post_type() );
-			endif;
-		}
-	}
+    /**
+     * Custom render function for Infinite Scroll
+     */
+    public static function infinite_scroll_render() {
+        while ( have_posts() ) {
+            the_post();
+            if ( is_search() ) :
+                get_template_part( 'template-parts/content', 'search' );
+            else :
+                get_template_part( 'template-parts/content', get_post_type() );
+            endif;
+        }
+    }
 }
 
 add_action( 'after_setup_theme', [ 'EmClient_Jetpack', 'setup' ] );
